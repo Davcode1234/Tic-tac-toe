290be1080929b934935f30e5fad9284412d14f1e
Dawid Pietruszewski <dpietruszewski12@gmail.com>
2022-09-18 17:03:51 +0200

    Upgrade modal content winning screen

--- js/app.js
+++ js/app.js
@@ -271,27 +271,50 @@ const handleModalOpen = (
   const restartModal = document.querySelector(".restart-modal");
   const restartContent = document.querySelector(".modal-content");
   const paragraph = document.querySelector(".modal-content--paragraph");
+  const decisionParagraph = document.querySelector(".decision-paragraph");
+  const mark = document.querySelector(".modal-content--img");
   const cancelBtn = document.querySelector(".cancel-cta-btn");
   const restartBtn = document.querySelector(".restart-cta-btn");
   const btnWrapper = document.querySelectorAll(".btn-wrapper button");
   const btnContent = ["QUIT", "NEXT ROUND"];
   const [left, right] = btnContent;
 
-  const addTextContent = (paragraphText, firstBtn, secondBtn) => {
+  const addTextContent = (
+    paragraphText,
+    decisionParagraphText,
+    firstBtn,
+    secondBtn,
+    markSrc
+  ) => {
     paragraph.textContent = paragraphText;
+    decisionParagraph.textContent = decisionParagraphText;
     cancelBtn.textContent = firstBtn;
     restartBtn.textContent = secondBtn;
+    mark.src = markSrc;
   };
+
   if (restart) {
-    addTextContent("RESTART GAME", "NO, CANCEL", "YES, RESTART");
+    addTextContent("RESTART GAME", "", "NO, CANCEL", "YES, RESTART");
   } else if (xWon) {
-    addTextContent("X TAKES THE ROUND", `${left}`, `${right}`);
+    addTextContent(
+      `TAKES THE ROUND`,
+      state.playerMark === "x" ? `You won!` : "Oops, you lost...",
+      `${left}`,
+      `${right}`,
+      `./images/icons/icon-x.svg`
+    );
     paragraph.style.color = `#31c3bd`;
   } else if (oWon) {
-    addTextContent("O TAKES THE ROUND", `${left}`, `${right}`);
+    addTextContent(
+      `TAKES THE ROUND`,
+      state.playerMark === "o" ? `You won!` : "Oops, you lost...",
+      `${left}`,
+      `${right}`,
+      `./images/icons/icon-o.svg`
+    );
     paragraph.style.color = `#f2b137`;
   } else if (draw) {
-    addTextContent("DRAW", `${left}`, `${right}`);
+    addTextContent("DRAW", "", `${left}`, `${right}`);
   }
   restartModal.classList.add("active-modal");
   restartContent.classList.add("active-modal-content");
